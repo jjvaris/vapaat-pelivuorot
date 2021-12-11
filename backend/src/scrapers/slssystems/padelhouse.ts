cbdb5cad2373683168f0c68297c5def82780b1d3
Jouni Varis <jjvaris@gmail.com>
2021-12-11 19:50:59 +0200

    fix padelhouse scraper

--- backend/src/scrapers/slssystems/padelhouse.ts
+++ backend/src/scrapers/slssystems/padelhouse.ts
@@ -26,7 +26,7 @@ const scrapePadelhouse = async (
       };
     })
     .get()
-    .filter((hour) => !hour.court.includes('Lounge'));
+    .filter((hour) => !hour.court.includes('Cupra'));
   return {
     hallId,
     day: hours[0]?.day ?? isoDate,
